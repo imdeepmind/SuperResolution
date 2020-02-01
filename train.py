1015cd455970c6f810395ac292c67063fe980775
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-02-02 01:42:01 +0530

    added model

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import os
 
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, BatchNormalization
+
 from logger import logger
 from dataset import Dataset
 
@@ -14,7 +18,21 @@ if not os.path.exists(SAVE_DIR + "/high/"):
 
 dataset = Dataset()
 
-print(GENERATE_DATASET)
-
 if GENERATE_DATASET:
+	logger.info('Generating the dataset')
 	dataset.generate_dataset(DATASET, SAVE_DIR)
+
+model = Sequential()
+
+model.add(Conv2D(filters=128, kernel_size= (9,9), padding= 'same', activation = 'relu', input_shape=(224, 224, 1)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=64, kernel_size= (5, 5), padding= 'same', activation = 'relu'))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=1, kernel_size= (5,5), padding= 'same', activation = 'linear'))
+
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+
+logger.info(model.summary())
+
